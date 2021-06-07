8ac38f564f89a8b37db514c4bb0ded04a3fcd8a4
Reid McCulloch <rmcc0724@gmail.com>
2021-06-06 23:55:11 -0700

    CSS Module Styles concluded

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -37,12 +37,14 @@ import styles from './CourseInput.module.css';
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('')
   const [isValid, setIsValid] = useState(true)
+  
   const goalInputChangeHandler = (event) => {
     if(event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value)
   }
+
   const formSubmitHandler = (event) => {
     event.preventDefault()
     if (enteredValue.trim().length === 0) {
@@ -54,7 +56,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={styles['form-control']}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input
           type="text"
